a650f1a9e4400c2ce2e28a618eaf6136eb732ad6
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-19 00:04:22 +0100

    Logo progress

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,19 @@ import Link from "next/link"
 import { Button } from "./ui/button"
 const header = () => {
   return (
-    <header>header</header>
+    
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto">
+        
+        {/* logo */}
+        <Link href="/">
+            <h1>
+                Jaiwin <span>.</span>
+            </h1>
+        </Link>
+        
+        </div>
+    </header>
   )
 }
 
