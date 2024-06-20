5a8d1e84e3fb5333aac81b63544d19e9e4d0b247
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-21 00:51:26 +0100

    Transition bug

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,4 +1,4 @@
-import { animate, motion } from "framer-motion"
+import { animate, motion } from "framer-motion";
 
 const stairAnimation = {
     initial: {
@@ -8,7 +8,7 @@ const stairAnimation = {
         top: "100%",
     },
     exit: {
-        top: ["100%, 0%"],
+        top: ["100%", "0%"],
     },
 }
 
@@ -18,27 +18,27 @@ const reverseIndex = (index) => {
 }
 
 const Stairs = () => {
-  return (
-    <>
-    {[...Array(6)].map((_, index)=> {
-        return (
-        <motion.div
-        key={index}
-        variants={stairAnimation}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{
-            duration: 0.4,
-            ease: "easeInOut",
-            delay: reverseIndex(index) * 0.1,
-        }}
-        className="h-full w-full bg-white relative"
-        />
+    return (
+        <>
+            {[...Array(6)].map((_, index) => {
+                return (
+                    <motion.div
+                        key={index}
+                        variants={stairAnimation}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        transition={{
+                            duration: 0.4,
+                            ease: "easeInOut",
+                            delay: reverseIndex(index) * 0.1,
+                        }}
+                        className="h-full w-full bg-white relative"
+                    />
+                );
+            })}
+        </>
     );
-    })}
-    </>
-  )
-}
+};
 
-export default Stairs
+export default Stairs;
