cb1b317395cfcdb7d471494d1be485f4fbd08736
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-21 00:21:02 +0100

    Page transition configurations

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -12,10 +12,29 @@ const stairAnimation = {
     },
 }
 
+const reverseIndex = (index) => {
+    const totalSteps = 6;
+    return totalSteps - index - 1;
+}
+
 const Stairs = () => {
   return (
     <>
-
+    {[...Array(6).map((_, index)=> {
+        <motion.div
+        key={index}
+        variants={stairAnimation}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: reverseIndex(index) * 0.1,
+        }}
+        className="h-full w-full bg-white relative"
+        />
+    })]}
     </>
   )
 }
