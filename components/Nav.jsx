e41d5715120028998e84d6c6395bee8f9f693d65
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-20 09:53:24 +0100

    Routing

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "home",
+        path: "/"
+    },
+    {
+        name: "skills",
+        path: "/skills"
+    },
+    {
+        name: "projects",
+        path: "/projects"
+    },
+    {
+        name: "about",
+        path: "/about"
+    },
+]
+
 const Nav = () => {
   return (
     <nav>desktop nav</nav>
