181b8345df8c9afeebf2fe62a4fe61d093c50292
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-20 10:01:57 +0100

    Navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -20,11 +20,27 @@ const links = [
         name: "about",
         path: "/about"
     },
+    {
+        name: "contact",
+        path: "/contact"
+    },
 ]
 
 const Nav = () => {
+    const pathname = usePathname();
+    console.log(pathname);
   return (
-    <nav>desktop nav</nav>
+    <nav className="flex gap-8">
+        {links.map((link, index) => {
+            return (
+                <Link href={link.path} key={index} className={`${
+                    link.path === pathname && "text-accent border-b-2 border-accent"
+                } capitalize font-medium hover:text-accent transition-all`}>
+                {link.name}
+                </Link>
+            )
+        })}
+    </nav>
   )
 }
 
