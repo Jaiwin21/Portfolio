09cd1c9bf9273f940fe4e5b155e1053d438015d8
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-22 23:54:47 +0100

    Photo on landing page#

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Photo from "@/components/ui/Photo";
 import Social from "@/components/ui/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
@@ -25,11 +26,11 @@ const Home = () => {
               <FiDownload className="text-xl" />
             </Button>
             <div className="mb-8 xl:mb-0">
-              <Social />
+              <Social containerStyles="flex gap-6" iconStyles="h-9 w-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
             </div>
           </div>
         </div>
-        <div>photo</div>
+        <Photo />
       </div>  
       
       </div>
