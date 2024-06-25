1c364c60f1e54ef85bf654ac794b58b72316d90c
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-25 11:08:29 +0100

    contact form in progress

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -34,10 +34,32 @@ import { Icon } from "@radix-ui/react-select";
     },
   ]
 
+  import { motion } from "framer-motion";
+
 
 const Contact = () => {
   return (
-    <div>contacts page</div>
+    <motion.section
+    initial={{opacity:0}}
+    animate={{
+      opacity:1,
+      transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}
+    }}
+    className="py-6"
+    >
+      <div className="container mx-auto">
+      <div className="flex flex-col xl:flex-row gap-[30px]">
+        <div className="xl:h-[54%] order-2 xl:order-none">
+          <form className="flex flex-col gap-6 py-10 bg-[#27272c] rounded-xl">
+            <h3 className="text-4xl text-accent">Let's work together</h3>
+            <p></p>
+          </form>
+        </div>
+        <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+      </div>
+      </div>
+
+      </motion.section>
   )
 }
 
