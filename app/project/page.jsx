cbced2c20ee8a13164162c5aacfe683cde0bff34
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-25 09:13:54 +0100

    Additions

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
+
 import {
   Tooltip, 
   TooltipContent,
@@ -16,10 +17,10 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 
-const Projects = [
+const projects = [
   {
     num:"01",
-    category:"frontend",
+    category:"Frontend",
     title:"project 1",
     description:"Ripple is a social media platform that mimics the likeness of Twitter. However, unlike Twitter, it aims to improve the quality of what is being posted as well as utilising AI to ensure the user is permitted to explore the platform. So far, the frontend is the only part that is complete.",
     stack: [{name:"HTML 5"}, {name:"Tailwind"}, {name:"React"}, {name:"NodeJs"}],
@@ -49,10 +50,61 @@ const Projects = [
   // }
 ]
 
-const Project = () => {
+const work = () => {
+  const [project, setProject] = useState(projects[0]);
   return (
-    <div>projects page</div>
+    <motion.section
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+            <div
+              className="text-8xl leading-none font-extrabold"
+              style={{
+                WebkitTextStroke: "2px white", 
+                color: "transparent", 
+              }}
+            >
+              {project.num}
+            </div>
+              <h2 className="text=[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length -1 && ","}
+                      </li>
+                  )
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div>
+                <Link href={project.github}>
+                <TooltipProvider delayDuration={100}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <BsArrowUpRight />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Live Project</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%] ">slider</div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
 
-export default Project
+export default work
