587a80d7ae00f9a712449330d2f94d1c0d02991f
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-25 12:13:03 +0100

    contact finished

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -14,7 +14,7 @@ import {
   } from "@/components/ui/select";
 
   import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
-import { Icon } from "@radix-ui/react-select";
+  import { Icon } from "@radix-ui/react-select";
 
   const info = [
     {
@@ -49,7 +49,7 @@ const Contact = () => {
     >
 <div className="container mx-auto">
   <div className="flex flex-col xl:flex-row gap-[30px]">
-    <div className="xl:h-[54%] order-2 xl:order-none">
+    <div className="xl:w-[54%] order-2 xl:order-none">
       <form className="flex flex-col gap-6 py-10 bg-[#27272c] rounded-xl">
         <h3 className="text-4xl text-accent p-4">Let's work together</h3>
         <p className="text-white/60 p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti omnis, fugiat maiores inventore.</p>
@@ -58,8 +58,10 @@ const Contact = () => {
           <Input type="text" placeholder="Lastname"/>
           <Input type="email" placeholder="Email"/>
           <Input type="tel" placeholder="Phone"/>
-          
-          <Select>
+        </div>
+
+        <div className="px-4">
+        <Select>
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Reason for contacting"/>
             </SelectTrigger>
@@ -72,7 +74,6 @@ const Contact = () => {
               </SelectGroup>
             </SelectContent>
           </Select>
-        
         </div>
 
         <div className="p-4">
@@ -81,11 +82,31 @@ const Contact = () => {
         placeholder="Type your message here."
         />
         </div> 
+        
+        <div className="px-4">
+        <Button size="md" className="max-w-40">
+        Send message
+        </Button>
+        </div>
 
       </form>
     </div>
     <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-      {/* Placeholder for info */}
+      <ul className="flex flex-col gap-10">
+        {info.map((item, index) => {
+          return (
+            <li key={index} className="flex items-center gap-6">
+              <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                <div className="text-[28px]">{item.icon}</div>
+              </div>
+              <div className="flex-1">
+                <p className="text-white/60">{item.title}</p>
+                <h3 className="text-xl">{item.description}</h3>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   </div>
 </div>
