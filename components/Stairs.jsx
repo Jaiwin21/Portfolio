1f9c4ba30a6e3078fc2f25b5dd916e9114e8a4fa
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-07-22 23:39:59 +0100

    optimising stairs file

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,49 +1,43 @@
-import { animate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const stairAnimation = {
-    initial: {
-        top: "0%",
-    },
-    animate: {
-        top: "100%",
-    },
-    exit: {
-        top: ["100%", "0%"],
-    },
+  initial: { top: "0%" },
+  animate: { top: "100%" },
+  exit: { top: ["100%", "0%"] },
 };
 
-const reverseIndex = (index) => {
-    const totalSteps = 6;
-    return totalSteps - index - 1;
-};
+const totalSteps = 6;
 
 const Stairs = () => {
-    return (
-        <>
-            {[...Array(6)].map((_, index) => {
-                return (
-                    <motion.div
-                        key={index}
-                        variants={stairAnimation}
-                        initial="initial"
-                        animate="animate"
-                        exit="exit"
-                        transition={{
-                            duration: 0.4,
-                            ease: "easeInOut",
-                            delay: reverseIndex(index) * 0.1,
-                        }}
-                        style={{
-                            height: "100%",
-                            width: "100%",
-                            backgroundColor: "rgb(0, 255, 153)",
-                            position: "relative"
-                        }}
-                    />
-                );
-            })}
-        </>
-    );
+  return (
+    <>
+      {Array.from({ length: totalSteps }, (_, index) => {
+        // Calculate delay for each step
+        const delay = (totalSteps - index - 1) * 0.1;
+
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay,
+            }}
+            style={{
+              height: "100%",
+              width: "100%",
+              backgroundColor: "rgb(0, 255, 153)",
+              position: "relative",
+            }}
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default Stairs;
